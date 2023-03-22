f4f75bb3c149f1a87f0026eb7253b4453c8b47f4
Andre Maute <andre.maute@sodgeit.de>
2023-03-22 18:11:45 +0100

    cleanup windowsenv.bat

--- windowsenv.bat
+++ windowsenv.bat
@@ -2,10 +2,3 @@ echo on
 
 set PATH=%PATH%;C:\Program Files\CMake\bin
 
-set PATH=%PATH%;C:\Program Files\Python39
-
-set BOOST_VERSION=1.72.0
-set BOOST_MSVC_VERSION=msvc-14.2
-set BOOST_ROOT=C:\local\boost_1_72_0
-set PATH=%BOOST_ROOT%\lib64-%BOOST_MSVC_VERSION%;%PATH%
-
