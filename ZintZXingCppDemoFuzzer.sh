20509b393fa75daab87641bf6ddc96024106e516
Andre Maute <andre.maute@sodgeit.de>
2023-11-20 17:12:03 +0100

    add corpus directory for fuzzer

--- ZintZXingCppDemoFuzzer.sh
+++ ZintZXingCppDemoFuzzer.sh
@@ -22,5 +22,5 @@ cmake -E rm -rf         ${BINARY_DIR}
 cmake -E make_directory ${BINARY_DIR}
 cmake -E chdir          ${BINARY_DIR} cmake -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} ${CMAKE_ARGS} -DFUZZER_MODE=On ${SOURCE_DIR}
 cmake -E chdir          ${BINARY_DIR} cmake --build . --config ${CMAKE_BUILD_TYPE}
-cmake -E chdir          ${BINARY_DIR}/src ./zintzxingcppdemo-test
+cmake -E chdir          ${BINARY_DIR}/src ./zintzxingcppdemo-test ${SOURCE_DIR}/fuzzer_corpus
 
