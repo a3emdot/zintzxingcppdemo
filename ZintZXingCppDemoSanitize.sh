6d857e52b5b87e789cad68c736566594c30e76c2
Andreas Lay <andreas.lay@sodgeit.de>
2024-05-16 12:05:06 +0200

    don't launch cmake from cmake, make a separate call in shell script

--- ZintZXingCppDemoSanitize.sh
+++ ZintZXingCppDemoSanitize.sh
@@ -24,6 +24,9 @@ cmake -E make_directory ${BINARY_DIR}/deps
 cmake -E copy  ${SOURCE_DIR}/deps/CMakeLists.txt ${BINARY_DIR}/deps/CMakeLists.txt
 cmake -E make_directory ${BINARY_DIR}/deps/build
 
+cmake -E chdir ${BINARY_DIR}/deps/build cmake -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} ..
+cmake -E chdir ${BINARY_DIR}/deps/build cmake --build . --config ${CMAKE_BUILD_TYPE}
+
 cmake -E chdir ${BINARY_DIR} cmake -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} ${CMAKE_ARGS} -DSANITIZE_MODE=On ${SOURCE_DIR}
 cmake -E chdir ${BINARY_DIR} cmake --build . --config ${CMAKE_BUILD_TYPE}
 
