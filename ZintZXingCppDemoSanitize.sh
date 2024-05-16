5e53abcaf4c5f3cf2607e0537b044e26b862033e
Andreas Lay <andreas.lay@sodgeit.de>
2024-05-16 12:00:39 +0200

    move mkdir from cmakelists into batchscript

--- ZintZXingCppDemoSanitize.sh
+++ ZintZXingCppDemoSanitize.sh
@@ -22,6 +22,7 @@ cmake -E rm -rf         ${BINARY_DIR}
 cmake -E make_directory ${BINARY_DIR}/deps
 
 cmake -E copy  ${SOURCE_DIR}/deps/CMakeLists.txt ${BINARY_DIR}/deps/CMakeLists.txt
+cmake -E make_directory ${BINARY_DIR}/deps/build
 
 cmake -E chdir ${BINARY_DIR} cmake -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} ${CMAKE_ARGS} -DSANITIZE_MODE=On ${SOURCE_DIR}
 cmake -E chdir ${BINARY_DIR} cmake --build . --config ${CMAKE_BUILD_TYPE}
